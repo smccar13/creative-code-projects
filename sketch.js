1040091a48ef9157ea8841625d2cd6dffe47aa38
smccar13 <168107725+smccar13@users.noreply.github.com>
2025-08-27 16:04:35 -0500

    update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,33 @@
-console.log("hello")
+//console.log("hello")
+
+let y = 0;
+let speed = 4;
 
 function setup(){
 createCanvas(windowWidth,windowHeight)
+background (255, 246, 161)
 }
 
 function draw(){
-background (220,)
+  let diameter = 40;
+  for(let i = diameter/2; i < width; i+=diameter) {
+//red value
+  let r = map(mouseX, 0, width, 0, 255)
+  
+  //green value
+  let g = map(mouseY, 0, height, 0, 255)
+  
+  let size = map(mouseX*mouseY, 0, width*height, 2, 40)
+  
+  fill(r, g, 150)
+  noStroke()    
+    ellipse(i, y, 20);
+  }
+  
+  if(y > height + diameter){
+    y = 0;
+  }
+  y+=speed;
 }
 
 function windowResized() {
