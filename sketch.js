a1b8454e9ce38e1f52dedc321a96045e8104f0db
smccar13 <168107725+smccar13@users.noreply.github.com>
2025-08-29 14:10:15 -0500

    added flowers, changed colors

--- sketch.js
+++ sketch.js
@@ -4,17 +4,43 @@ let rad = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(240, 218, 224);
+  background(195, 247, 148);
+}
+ 
+// Draw flower each time mouse is clicked
+function mousePressed() {
+//set flower size and number of petals
+  let petalAngle = TWO_PI/7;
+  let flowerSize = random(80, 200)
+//random location
+  let x = random(0, width);
+  let y = random(0, height);
+
+  push();
+  translate(x, y);
+  
+  fill(255, random(0, 255), random(0, 255), 99);
+  for (let i = 0; i < 7; i++) {
+    ellipse(0, 0, flowerSize, flowerSize/4);
+    rotate(petalAngle);
+  }
+
+  // flower center
+  fill(255, random(0, 255), random(0, 60));
+  circle(0, 0, flowerSize/3.5);
+
+  pop();
 }
 
 function draw() {
-  //green value
-  let g = map(mouseX, 0, width, 100, 255)
-  //blue value
-  let b = map(mouseY, 150, height, 0, 200)
-  translate(width/2, height/2);
   noStroke();
-  fill(255, g, b);
+  //red value
+  let r = map(mouseX, 0, width, 70, 123);
+  //blue value
+  let b = map(mouseY, 150, height, 0, 111);
+  push();
+  translate(width/2, height/2);
+  fill(r, 128, b, 50);
   let x = rad * cos(angle);
   let y = rad * sin(angle);
   circle(x, y, 20);
@@ -25,12 +51,13 @@ function draw() {
   //clear screen when spiral fills window
     let maxRad = dist(0, 0, width/2, height/2);
   if (rad > maxRad) {
-    background(240, 218, 224);
+    background(195, 247, 148);
     rad = 0;
     angle = 0;
   }
+  pop();
 }
 
 function windowResized() {
-    resizeCanvas(windowWidth, windowHeight)
+    resizeCanvas(windowWidth, windowHeight);
 }
